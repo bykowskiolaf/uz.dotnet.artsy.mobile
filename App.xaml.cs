dce25382209f2454dcd8c37d4a62ac576ee1ddde
Olaf Bykowski <olafbykowski1@gmail.com>
2025-04-07 09:47:44 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace artsy.mobile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace artsy.mobile;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
